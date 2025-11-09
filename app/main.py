f2795bb0beb41e266e0d20cabc0bc2f94f65034c
Nima77Moosavi <94492258+Nima77Moosavi@users.noreply.github.com>
2025-11-09 14:18:10 +0330

    routers

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, APIRouter
+from app.users.routers import router as users_router
+
 
 app = FastAPI()
 
 router = APIRouter()
 
 # Correct usage: call include_router, not decorate
-app.include_router(router)
+app.include_router(users_router)
 
 @app.get("/")
 async def health():
